b7d48d2d0f0a57dc7bb543b43c1a350fdb51b7be
yookyung Choi <ykc131719@gmail.com>
2021-01-14 03:06:06 +0900

    Add Button and input Box

--- FirstApplication/app/src/main/java/com/example/firstapplication/MainActivity.java
+++ FirstApplication/app/src/main/java/com/example/firstapplication/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.firstapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText et_id;
+    Button btn_test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        et_id = findViewById(R.id.et_id);
+        btn_test = findViewById(R.id.btn_test);
+
+        btn_test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                et_id.setText("유키");  //버튼을 클릭하면 EditText 안에 "유키"라는 이름을 적어줘라
+            }
+        });
     }
 }
