a093849b06f55dd08aee7a0185827c2b6b87a8b3
yookyung Choi <ykc131719@gmail.com>
2021-01-14 03:11:04 +0900

    Add Button and Input Box

--- FirstApplication/app/src/main/java/com/example/firstapplication/MainActivity.java
+++ FirstApplication/app/src/main/java/com/example/firstapplication/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         btn_test.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                et_id.setText("유키");  //버튼을 클릭하면 EditText 안에 "유키"라는 이름을 적어줘라
+                et_id.setText("유디");  //버튼을 클릭하면 EditText 안에 "유키"라는 이름을 적어줘라
             }
         });
     }
